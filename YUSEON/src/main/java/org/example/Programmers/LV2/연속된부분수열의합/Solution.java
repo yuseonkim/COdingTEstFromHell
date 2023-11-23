a3e609cc2822194fd7445353fc6ee19d746036d3
yuseonkim <yusun715@icloud.com>
2023-11-23 23:52:42 +0900

    23.11.23

--- YUSEON/src/main/java/org/example/Programmers/LV2/연속된부분수열의합/Solution.java
+++ YUSEON/src/main/java/org/example/Programmers/LV2/연속된부분수열의합/Solution.java
@@ -1,38 +1,53 @@
-package org.example.Programmers.LV2.연속된부분수열의합;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Solution {
-    public static int[] solution(int[] sequence, int k) {
-        List<Integer> indexs = new ArrayList<>();
-        sequence = Arrays.stream(sequence).filter(i -> i <= k).toArray();
-        int[] answer = new int[2];
-        int sum = 0;
-        int saveIndex = 0;
-        for (int i = 0; i < sequence.length; i++) {
-            System.out.println(i);
-            sum += sequence[i];
-            indexs.add(i);
-            if (sum > k) {
-                indexs.clear();
-                sum = 0;
-                i = saveIndex;
-                saveIndex++;
-            }
-            if (sum == k) {
-                answer[0] = indexs.get(0);
-                answer[1] = indexs.get(indexs.size()-1);
-                indexs.clear();
-                sum = 0;
-            }
-        }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        int[] sequence = {1, 1, 1, 2, 3, 4, 5};
-        int k = 5;
-    }
-}
+//package org.example.Programmers.LV2.연속된부분수열의합;
+//
+//import java.util.ArrayList;
+//
+//public class Solution {
+//    public static int[] solution(int[] sequence, int k) {
+//        int start = 0;
+//        int end = 0;
+//        int sum = sequence[0];
+//        int size = 1000000;
+//        int[] result = new int[2];
+//        ArrayList<int[]> resultList = new ArrayList<>();
+//        while (true) {
+//            System.out.println(sum + " start : " + start + " end : " + end);
+//            if (sum < k) {
+//                end++;
+//                sum += sequence[end];
+//                continue;
+//            }
+//            if (sum > k) {
+//                sum -= sequence[start];
+//                start++;
+//                continue;
+//            }
+//            if (sum == k) {
+//                if (end - start <= size) {
+//                    size = end - start;
+//                    result = new int[]{start, end};
+//                }
+//                if (end == sequence.length - 1) {
+//                    sum -= sequence[start];
+//                    start++;
+//                    continue;
+//                }
+//                end++;
+//                sum += sequence[end];
+//            }
+//        }
+//
+//        return result;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        int[] sequence = {2, 2, 2, 2, 2};
+//        int k = 7;
+//        int[] result = solution(sequence, 6);
+//        for (int i : result) {
+//            System.out.print(i + " ");
+//        }
+//    }
+//}
+//
