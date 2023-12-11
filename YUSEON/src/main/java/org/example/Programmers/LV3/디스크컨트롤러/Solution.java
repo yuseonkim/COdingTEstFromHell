8fdd27a7f7d8fcde2431fad742146d57536eceb0
yuseonkim <yusun715@icloud.com>
2023-12-12 01:48:13 +0900

    23.12.12

--- YUSEON/src/main/java/org/example/Programmers/LV3/디스크컨트롤러/Solution.java
+++ YUSEON/src/main/java/org/example/Programmers/LV3/디스크컨트롤러/Solution.java
@@ -1,26 +1,26 @@
-package org.example.Programmers.LV3.디스크컨트롤러;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
-public class Solution {
-    public int solution(int[][] jobs) {
-        int[][] sortedJobs = Arrays.stream(jobs).sorted((a, b) -> Integer.compare(a[0], b[0])).toArray(int[][]::new);
-        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
-            }
-        });
-
-        queue.offer(jobs[0]);
-        int sum = 0;
-        int idx = 1;
-
-        while(!queue.isEmpty()){
-
-        }
-    }
-}
+//package org.example.Programmers.LV3.디스크컨트롤러;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Comparator;
+//import java.util.PriorityQueue;
+//
+//public class Solution {
+//    public int solution(int[][] jobs) {
+//        int[][] sortedJobs = Arrays.stream(jobs).sorted((a, b) -> Integer.compare(a[0], b[0])).toArray(int[][]::new);
+//        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                return o1[1] - o2[1];
+//            }
+//        });
+//
+//        queue.offer(jobs[0]);
+//        int sum = 0;
+//        int idx = 1;
+//
+//        while(!queue.isEmpty()){
+//
+//        }
+//    }
+//}
